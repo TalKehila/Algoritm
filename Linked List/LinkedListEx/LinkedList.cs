6c2c5dcff583e4dd2df98b208e2637d7806cb218
TalKehila <129615478+TalKehila@users.noreply.github.com>
2023-08-30 14:26:33 +0300

    How to use Linked List with node

---
+++ Linked List/LinkedListEx/LinkedList.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListEx
+{
+    public class LinkedList
+    {
+        public class Node
+        {
+            public int key;
+            public Node next;
+            public Node(int key)
+            {
+                this.key = key;
+            }
+        }
+
+        public Node head;
+        public LinkedList()
+        {
+            head = null;
+        }
+        public LinkedList(params int[] keys)
+        {
+            Array.Sort(keys);
+            head = new Node(keys[0]);
+            Node temp = head.next;
+            for (int i = 1; i < keys.Length; i++)
+            {
+                if (keys[i] != keys[i - 1])
+                {
+                    temp = new Node(keys[i]);
+                    temp = temp.next;
+                }
+            }
+        }
+        public LinkedList(LinkedList other)
+        {
+            Node tempThis = this.head;
+            Node tempOther = other.head;
+            while (tempOther != null)
+            {
+                tempThis = new Node(tempOther.key);
+
+                tempThis = tempThis.next;
+                tempOther = tempOther.next;
+            }
+        }
+
+        public void Insert(int key)
+        {
+            Node newNode = new Node(key);
+            if (head == null || key < head.key)
+            {
+                newNode.next = head;
+                head = newNode;
+                return;
+            }
+
+            Node temp = head;
+            while (temp.next != null && temp.next.key < key)
+            {
+                temp = temp.next;
+            }
+
+            if (temp.next != null && temp.next.key == key)
+                return;
+
+            newNode.next = temp.next;
+            temp.next = newNode;
+        }
+        public void Delete(int key)
+        {
+            if (head != null)// if i have number in my linked list 
+            {
+                if (head.key == key)// this if delete it if the key is the same number as head and do head ++ after delete
+                {
+                    head = head.next;
+                    return;
+                }
+                Node nexthead = head.next;// new index (my next index)
+                Node headnow = head;// new index (my now index)
+                while (nexthead != null && nexthead.key != key) // if the next member is not null and not the key 
+                {
+                    headnow = nexthead;// update  for keep checking and process the next node
+                    nexthead = nexthead.next;
+                }
+                if (nexthead != null)// if not null so keep serching
+                {
+                    headnow.next = nexthead.next;
+                }
+            }
+        }
+        public static LinkedList Merge(LinkedList firstlist, LinkedList otherlist)
+        {
+            LinkedList mergelist = new LinkedList();
+
+            Node node1 = firstlist.head;
+            Node node2 = otherlist.head;
+            while (node1 != null && node2 != null) //ruuning until 1 of the list is empty
+            {
+                if (node1.key < node2.key)
+                {
+                    mergelist.Insert(node1.key);// key will insert into the the merge else node 2 key will do it 
+                    node1 = node1.next;
+                }
+                else
+                {
+                    mergelist.Insert(node2.key);
+                    node2 = node2.next;
+                }
+            }
+            while (node1 != null)
+            {
+                mergelist.Insert(node1.key);
+                node1 = node1.next;
+            }
+            while (node2 != null)
+            {
+                mergelist.Insert(node2.key);
+                node2 = node2.next;
+            }
+            return mergelist;
+        }
+        public override string ToString()
+        {
+            Node temp = head;
+            string str = "[";
+            while (temp != null)
+            {
+                if (temp != head)
+                    str += ",";
+
+                str += temp.key;
+                temp = temp.next;
+            }
+            str += "]";
+            return str;
+        }
+
+        
+    }
+}
---
+++ Linked List/LinkedListEx/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            
+
+            LinkedList list2 = new LinkedList();
+            LinkedList list1 = new LinkedList();
+
+            list1.Insert(5);         
+            list1.Insert(6);
+            list1.Insert(7);
+            list1.Insert(8);
+            list1.Insert(13);
+            list1.Insert(10);
+            list1.Insert(11);
+            list1.Insert(12);
+
+            list1.Delete(10); // delete
+
+            list2.Insert(14);
+            list2.Insert(15);
+            list2.Insert(16);
+
+           /* Console.WriteLine(new LinkedList().ToString());*/// option to call a empty list 
+            Console.WriteLine("list 1 : " + list1);
+            Console.WriteLine("list 2 : " + list2);
+            LinkedList mergedList = LinkedList.Merge(list1, list2);
+            Console.WriteLine("Merged List: " + mergedList);
+
+            // section 4
+            LinkedList exmplelist1 = new LinkedList();
+            LinkedList exmplelist2 = new LinkedList();
+
+            for (int i = 1; i <= 9; i+=2)
+            {
+                exmplelist1.Insert(i);
+            }
+            for (int i = 0; i <= 8; i +=2)
+            {
+                exmplelist2.Insert(i);
+            }
+            LinkedList mergedexmpleList = LinkedList.Merge(exmplelist1, exmplelist2);
+
+            Console.WriteLine(" exmple list 1 :" + exmplelist1);
+            Console.WriteLine(" exmple list 2 :" + exmplelist2);
+            Console.WriteLine(" merge of the two exmple list will start now");
+            if (mergedexmpleList != null)
+            {
+                Console.WriteLine("Success : " + mergedexmpleList);
+            }
+            else
+            {
+                Console.WriteLine(" nope it didnt success");
+            }
+
+
+
+        }
+    }
+
+
+}
+
