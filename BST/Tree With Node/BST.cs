e0fa41beea0cacc9f6a67fe51696602c2be480b6
TalKehila <129615478+TalKehila@users.noreply.github.com>
2023-08-30 14:21:12 +0300

    BST Exe

---
+++ BST/Tree With Node/BST.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree_With_Node
+{
+    internal class BST
+    {
+        public Node root { get; set; }
+
+        public void Add(int value)
+        {
+            Node newNode = new Node() { Value = value };
+            if (root == null)
+            {
+                root = newNode;
+                return;
+            }
+            Node tmpNode = root;
+            while (true)
+            {
+                if (value < tmpNode.Value)
+                {
+                    if (tmpNode.Left == null)
+                    {
+                        tmpNode.Left = newNode;
+                       
+                        return;
+                    }
+                    tmpNode = tmpNode.Left;
+
+
+
+                }
+                if (value >= tmpNode.Value)
+                {
+                    if (tmpNode.Right == null)
+                    {
+                        tmpNode.Right = newNode;
+                        return;
+                    }
+                    tmpNode = tmpNode.Right;
+                }
+            }
+
+        }
+
+        public void ScanInOrder() => ScanInOrder(root);
+
+        public void ScanInOrder(Node node)
+        {
+            if (node == null) return;
+            ScanInOrder(node.Left);
+            Console.WriteLine(node.Value);
+            ScanInOrder(node.Right);
+
+        }
+
+    }
+}
+
---
+++ BST/Tree With Node/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tree_With_Node
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BST  bST = new BST();
+            bST.Add(10);
+            bST.Add(5);
+            bST.Add(8);
+            bST.Add(19);
+            bST.Add(123);
+            bST.Add(85);
+
+            bST.ScanInOrder();
+
+        }
+    }
+}
