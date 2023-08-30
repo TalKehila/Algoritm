b41c4b2d5302ed4183307b5fe4def5fde277c016
TalKehila <129615478+TalKehila@users.noreply.github.com>
2023-08-30 14:33:40 +0300

    Recursion Recursion Recursion

---
+++ Recursion/RecursionsExeArray6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecursionsExeArray6
+{
+    internal class Program
+    {
+        // רקורסיה שמקבלת מערך ומחזירה את המספר המקסימלי
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine(IsPolindrom("12345",0));
+            Console.WriteLine(IsPolindrom("12321",0));
+            Console.WriteLine(IsPolindrom("2468",0));
+            Console.WriteLine(IsPolindrom("268862", 0));
+
+        }
+        public static int PrintMaxNumber(int[]arr, int index)
+        {
+            if(index== arr.Length-1)
+            {
+                return arr[index];
+            }         
+           return Math.Max(arr[index],PrintMaxNumber(arr, index + 1));
+        }
+        public static bool IsPolindrom(string someting,int index=0)
+        {
+            if (index==someting.Length/2)
+            {
+                return true;
+            }
+            if (someting[index] == someting[someting.Length-1-index])
+            {
+                return
+                IsPolindrom(someting,index+1);
+                
+            }
+            return false;
+        }
+    }
+}
---
+++ Recursion/ecercise algorithms 14-20/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ecercise_algorithms_14_20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[,] myarr = { { 1, 5, 5, 2 },{ 5, 3, 2, 1 } };
+            int count = 0;
+            int result = NumTimesInTwoDArray(myarr,count,0,0);
+            Console.WriteLine(result);
+
+        }
+        // exercise 12 
+        public static int SumDigit(int n)
+        {
+            if (n < 10)
+            {
+                return n;
+            }
+            int lastDigit = n % 10;
+            int remainigDigits = n / 10;
+            int mulDigit = SumDigit(remainigDigits);
+            return lastDigit * mulDigit;
+        }
+        // exercise 13
+        public static int PrintOddNumber(int n)
+        {
+            if (n < 0)
+            {
+                return n;
+            }
+            int lastDigit = n % 10;
+            int remainigDigits = n / 10;
+
+            if (n % 2 == 1)
+            {
+                Console.WriteLine(lastDigit);
+            }
+            PrintOddNumber(remainigDigits);
+            return n;
+
+
+        }
+        // exerice 14 
+        public static void printOddInArr(int[] arr, int index=0)
+        {
+            if (arr == null || index == arr.Length)
+            {
+                return;
+            }
+            if (index % 2 == 1)
+            {
+                Console.WriteLine(arr[index]);
+            }
+            printOddInArr(arr, index + 1);
+        }
+        // exercise 15 
+        public static bool SortedArr(int[] arr, int i = 0)
+        {
+            if (arr == null || i == arr.Length - 1)
+            {
+                return true;
+            }
+            if (arr[i] >= arr[i + 1])
+            {
+                return false;
+            }
+           return SortedArr(arr, i+1);
+            
+        }
+        // exercise 16 
+       public static int NumTImes( int[]arr , int i = 0, int num=0,int count = 0) 
+        {
+            if (arr == null || i == arr.Length)
+            {
+                return count;
+            }
+            if (arr[i]==num)
+            {
+                count++;
+            }
+           return NumTImes(arr, i+1, num,count);
+           
+        }
+        // exericse 17
+        public static int NumTimesInTwoDArray(int[,] arr, int count ,int row, int col)
+        {    int num = 5;
+            if(arr == null || row ==arr.GetLength(0) || col == arr.GetLength(1))
+            {
+                return count;
+            }
+            if (arr[row,col] == num)
+            {
+                count++;
+            }
+            if (col +1 < arr.GetLength(1))
+
+            count= NumTimesInTwoDArray(arr, count, row, col+1);
+
+           if(row+1 < arr.GetLength(0)) 
+
+             count= NumTimesInTwoDArray(arr,count, row+1, col);
+
+            return count;
+        }
+
+    }
+}
