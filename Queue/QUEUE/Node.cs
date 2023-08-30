b1f9a5a012bc0aa90b8749e0b31b736dfcb759ee
TalKehila <129615478+TalKehila@users.noreply.github.com>
2023-08-30 14:27:45 +0300

    What is a queue?

---
+++ Queue/QUEUE/Node.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUEUE
+{
+    class Queue <T>
+    {
+        Node<T> head;
+        Node<T> tail;
+
+        public void Insert(T item)
+        {
+            if(head == null)           {
+                head = new Node<T>(item);
+                tail = head;
+            }
+            else
+            {
+                Node < T > n = new Node<T>(item);
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public T Remove()
+        {
+            if (head == null)
+            {
+                throw new Exception("empty queue ");
+            }
+            T ans = head.Data;
+            head = head.Next;
+            return ans;
+        }
+        public T Pick()
+        {
+            if (head == null)
+            {
+                throw new Exception("empty queue");
+            }
+            T ans = head.Data;       
+            return ans;
+        }
+        public bool IsEmpty()
+        {
+            return head == null;
+        }
+        public override string ToString()
+        { 
+            Node <T> n = head;       
+            string s = "";
+            while(n != null)
+            {
+                s += n.ToString();
+                n = n.Next;
+            }
+            return s;
+        }
+    }
+    class Node<T>
+    {
+        T data;
+        Node<T> next;
+
+        public Node(T data)
+        {
+            this.data = data;
+        }
+        public Node(T data, Node<T> next)
+        {
+            this.next = next;
+            this.data = data;
+        }
+
+        public T Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+
+        public Node<T> Next
+        {
+            get { return next; }
+            set { next = value; }
+        }
+
+        public override string ToString()
+        {
+            return data.ToString();
+        }
+    }
+
+}
+
---
+++ Queue/QUEUE/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUEUE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+            queue.Insert(1);
+            Console.WriteLine(queue);
+            queue.Insert(2);
+            Console.WriteLine(queue);
+            queue.Insert(3);
+            Console.WriteLine(queue);
+            queue.Remove();
+            Console.WriteLine(queue);
+            queue.Remove();
+            Console.WriteLine(queue);
+            queue.Remove();
+            Console.WriteLine(queue);
+            queue.Insert(4);
+            Console.WriteLine(queue);
+        }
+    }
+}
