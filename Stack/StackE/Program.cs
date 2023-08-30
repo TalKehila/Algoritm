bce29c3aedbf70f5e632dd955d6e17a6d11e2737
TalKehila <129615478+TalKehila@users.noreply.github.com>
2023-08-30 14:29:05 +0300

    stack FIFO

---
+++ Stack/StackE/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] arr = {1,5,3};
+            //Console.WriteLine(SortPush(arr));
+            string s = "[([])]";
+            Console.WriteLine(S(s));
+        }
+        public static bool S(string someting)
+        {
+
+            if (someting == null)
+            {
+                return true;
+            }
+            Stack<char> mystack = new Stack<char>();
+
+            for (int i = 0; i < someting.Length; i++)
+
+            {
+                if (someting[i] == '[' || someting[i] == '{' || someting[i] == '(')
+                {
+                    mystack.Push(someting[i]);
+                }
+                if (someting[i] == ']' || someting[i] == '}' || someting[i] == ')')
+                {
+                    if (mystack.IsEmpty())
+                    {
+                        return false;
+                    }
+                    char c = mystack.Pop();
+                    if (someting[i] == ']')
+                    {
+                        if (c != '[')
+                        {
+                            return false;
+                        }
+                    }
+                    if (someting[i] == '}')
+                    {
+                        if (c != '{')
+                        {
+                            return false;
+                        }
+                    }
+                    if (someting[i] == ')')
+                    {
+                        if (c != '(')
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            if (mystack.IsEmpty())
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static int SortPush(int[] someting) 
+        { 
+            int count = 0;  
+            Stack<int> mystack = new Stack<int>();
+           
+            for (int i = 0; i < someting.Length; i++)
+            {              
+                if (mystack.IsEmpty() || someting[i] > mystack.Top())
+                {                 
+                    mystack.Push(someting[i]);
+                    count++;
+                }                           
+                else
+                {
+                    Stack<int> tempstack = new Stack<int>();
+                    while (!mystack.IsEmpty() && mystack.Top() > someting[i])
+                    {
+                        count++;
+                        tempstack.Push(mystack.Pop());
+                    }
+                    mystack.Push(someting[i]);
+                    while (!tempstack.IsEmpty())
+                    {
+                        count++;
+                        mystack.Push(tempstack.Pop());
+                    }
+                }            
+            }
+            Console.WriteLine(mystack);
+            return count;
+        }                  
+    }
+}
---
+++ Stack/StackE/Stack.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackE
+{
+    internal class Stack<T>
+    {
+        Node<T> head;
+
+        public void Push(T data)
+        {
+            if (head == null)
+            {
+                head = new Node<T>(data);
+                return;
+            }
+            else
+            {
+                head = new Node<T>(data, head);
+            }
+        }
+        public bool IsEmpty() { return head == null; }
+        public T Pop()
+        {
+            if (head == null)
+            {
+                throw new Exception("null stack is empty");
+            }
+            else
+            {
+                T ans = head.Data;
+                head = head.Next;
+                return ans;
+            }
+
+        }
+        public T Top()
+        {
+            if (head == null)
+            {
+                throw new Exception("null");
+            }
+            else
+            {
+                T ans = head.Data;
+                return ans;
+            }
+        }
+        public override string ToString()
+        {
+
+            Node<T> n = head;
+            string s = "";
+            while (n != null)
+            {
+                s += n.ToString();
+                n = n.Next;
+            }
+            return s;
+        }
+    }
+    class Node<T>
+    {
+        T data;
+        Node<T> next;
+
+        public Node(T data)
+        {
+            this.data = data;
+        }
+        public Node(T data, Node<T> next)
+        {
+            this.next = next;
+            this.data = data;
+        }
+        public T Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+        public Node<T> Next
+        {
+            get { return next; }
+            set { next = value; }
+        }
+        public override string ToString()
+        {
+            return data.ToString();
+        }
+    }
+
+}
+
+
+
